8ec280022a8da99910d52660c67a95145cae2cc5
Matheus Mendes <mt.mendes@outlook.com>
2023-06-21 20:06:33 -0300

    Configuração de CORS.

--- src/main/java/br/facens/plataformateologia/application/controller/CapituloController.java
+++ src/main/java/br/facens/plataformateologia/application/controller/CapituloController.java
@@ -4,14 +4,12 @@ import br.facens.plataformateologia.domain.model.dto.CapituloDetalheDTO;
 import br.facens.plataformateologia.domain.service.CapituloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.NoSuchElementException;
 
 @RestController
+@CrossOrigin
 @RequestMapping("capitulo")
 public class CapituloController {
 
--- src/main/java/br/facens/plataformateologia/application/controller/MaterialController.java
+++ src/main/java/br/facens/plataformateologia/application/controller/MaterialController.java
@@ -14,6 +14,7 @@ import java.util.NoSuchElementException;
 import java.util.Objects;
 
 @RestController
+@CrossOrigin
 @RequestMapping("material")
 public class MaterialController {
 
